73fd4caefd2a898b61d67aef2b746c405e9fa735
Diego Skieresz <diegoskieresz@gmail.com>
2023-01-21 14:06:27 -0300

    feat:adding header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,14 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
